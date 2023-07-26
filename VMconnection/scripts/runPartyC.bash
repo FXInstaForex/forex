5c4b242e47f072eeecd3ebb5c2fe3a06ec0c25c2
Ubuntu <azure_user@ubuntu-vm-test.u05d5cyqsapezgddhztqhjt4wg.ix.internal.cloudapp.net>
2023-07-26 03:01:19 +0000

    20230726

--- VMconnection/scripts/runPartyC.bash
+++ VMconnection/scripts/runPartyC.bash
@@ -1,8 +1,7 @@
 cd /etc/app/forex-v1/VMconnection/PartyC
 java -jar corda.jar &
 P1=$!
-#cd /etc/app/forex-v1;
-#./gradlew clean runPartyCServer &
-#P2=$!
-wait $P1 
-# $P2
+cd /etc/app/forex-v1;
+./gradlew clean runPartyCServer &
+P2=$!
+wait $P1 $P2
